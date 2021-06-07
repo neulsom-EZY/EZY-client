bbd5c8fe7e7a44d17bf9a50ede48d0e472134c89
4YJ <67637706+yejun0705@users.noreply.github.com>
2021-06-07 19:57:19 +0900

    리턴문 수정

--- ezy-client/src/Components/Section1/Section1.js
+++ ezy-client/src/Components/Section1/Section1.js
@@ -2,7 +2,9 @@ import React from "react";
 import { MainSection } from "./style";
 
 const Section1 = () => {
-  return <MainSection>This is First Page.</MainSection>;
+  return(
+    <MainSection>This is First Page.</MainSection>
+  );
 };
 
 export default Section1;
