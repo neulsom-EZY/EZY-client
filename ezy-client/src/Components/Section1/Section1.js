20fab41274f98ae8208b9fe1ba7c902b8499e457
4YJ <67637706+yejun0705@users.noreply.github.com>
2021-06-09 20:28:04 +0900

    매인 페이지 완성이요 하하

--- ezy-client/src/Components/Section1/Section1.js
+++ ezy-client/src/Components/Section1/Section1.js
@@ -7,9 +7,15 @@ const Section1 = () => {
     <>
       <S.MainSection>
         <S.TitleSection>
-          <S.MainTitle>
-            EZY - Client
-            <S.SubTitle>EZY는 자기 자신만의 라이프 스타일 역사를 쓰고 있습니다 😎</S.SubTitle>  
+          <S.MainTitle className="slider">
+	               EZY
+            <S.SubTitle>
+		          <div class="text-box">
+			          <div>자신만의 라이프스타일 역사를 쓰고 있습니다.</div>
+			          <div>여러분이 더 가치있는 삶을 살 수 있도록 합니다.</div>
+			          <div>라이프스타일을 혁신하며 우리를 변화시켜갑니다.</div>
+		            </div>
+            </S.SubTitle>
           </S.MainTitle>
         </S.TitleSection>
         <S.MainBg><img className="Bg01" src={Bg01}/></S.MainBg>
--- ezy-client/src/Components/Section1/style.js
+++ ezy-client/src/Components/Section1/style.js
@@ -15,11 +15,10 @@ export const MainBg = styled.div `
 
 export const TitleSection = styled.div `
     width: 100%;
-    height: 50vh;
+    height: 60vh;
     color: white;
     display:flex;
 `;
-
 export const MainTitle = styled.div `
     width: 100%;
     display: flex;
@@ -28,11 +27,124 @@ export const MainTitle = styled.div `
     justify-content: center;
     font-size: 75px;
     margin-top: 40vh;
+    font-weight: 500;
+
+.text-box{
+    width: 100%;
+	display: flex;
+    justify-content:center;
+    align-items:center;
+	position: relative;
+    margin-top:10vh;
+    font-size: 25px;
+}
+
+.text-box div{
+	display: inline-block;
+	position: absolute;
+	top: -200px;
+	transform: rotateX(-90deg);
+	opacity: 0;
+	text-shadow: 0px 5px 5px rgba(0,0,0,.25);
+	animation-timing-function: ease;
+}
+
+.text-box div:nth-child(1){
+	animation: rollDown 10s forwards infinite;
+}
+
+.text-box div:nth-child(2){
+	animation: rollDown2 10s forwards infinite;
+}
+
+.text-box div:nth-child(3){
+	animation: rollDown3 10s forwards infinite;
+}
+
+@keyframes rollDown {
+	0%{
+		top: -200px;
+		transform: rotateX(-90deg);
+	}
+	11%{
+		top: -74px;
+		transform: rotateX(0deg);
+		opacity: 1;
+	}
+	22%{
+		top: -74px;
+		transform: rotateX(0deg);
+		opacity: 1;
+	}
+	33%{
+		top: 50px;
+		transform: rotateX(30deg);
+		opacity: 0;
+	}
+}
+
+@keyframes rollDown2 {
+	33%{
+		top: -200px;
+		transform: rotateX(-90deg);
+	}
+	44%{
+		top: -74px;
+		transform: rotateX(0deg);
+		opacity: 1;
+	}
+	55%{
+		top: -74px;
+		transform: rotateX(0deg);
+		opacity: 1;
+	}
+	66%{
+		top: 50px;
+		transform: rotateX(30deg);
+		opacity: 0;
+	}
+}
+
+@keyframes rollDown3 {
+	66%{
+		top: -200px;
+		transform: rotateX(-90deg);
+	}
+	77%{
+		top: -74px;
+		transform: rotateX(0deg);
+		opacity: 1;
+	}
+	88%{
+		top: -74px;
+		transform: rotateX(0deg);
+		opacity: 1;
+	}
+	99%{
+		top: 50px;
+		transform: rotateX(30deg);
+		opacity: 0;
+	}
+}
+
+@keyframes slideColor{
+	0%{
+		background-color: #387780;
+	}
+	33%{
+		background-color: #3f88c5;
+	}
+	66%{
+		background-color: #588b8b;
+	}
+	100%{
+		background-color: #387780;
+	}
+}
 `;
 
 export const SubTitle = styled.div `
     width: 100%;
-    font-size: 20px;
     display:flex;
     flex-direction: column;
     align-items: center;
