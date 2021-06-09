49d9c070411f47f6618f5dde2c93bcfd6be5b010
4YJ <67637706+yejun0705@users.noreply.github.com>
2021-06-09 11:34:47 +0900

    가랏 기홍몬

--- ezy-client/src/Components/Section1/Section1.js
+++ ezy-client/src/Components/Section1/Section1.js
@@ -1,13 +1,18 @@
 import React from "react";
-import { MainSection } from "./style";
+import { MainBg, MainTitle, MainSection, SubTitle } from "./style";
+import Bg01 from '../../Assets/bg01.png';
 
 const Section1 = () => {
   return(
     <>
-      <MainSection>This is First Page.</MainSection>
+      <MainSection>
+        <MainTitle>EZY
+          <SubTitle>EZY는 자기 자신만의 라이프 스타일 역사를 쓰고 있습니다.</SubTitle>
+        </MainTitle>
+        <MainBg><img className="Bg01" src={Bg01}/></MainBg>
+      </MainSection>
     </>
   );
 };
-
 
 export default Section1;
--- ezy-client/src/Components/Section1/style.js
+++ ezy-client/src/Components/Section1/style.js
@@ -2,8 +2,33 @@ import styled from 'styled-components';
 
 export const MainSection = styled.div `
     display: flex;
-    justify-content: center;
-    align-items: center;
-    /* background-color: #6B40FF; */
+    justify-content: space-between;
+    width: 100%;
     color: white;
+    z-index: 1;
+    right: 0;
+`;
+
+export const MainBg = styled.div `
+    .Bg01 {
+        width: 110vh;
+        height: 100vh;
+    }
+`;
+
+export const MainTitle = styled.div `
+    width: 110vh;
+    display: flex;
+    justify-content: center;
+    color: white;
+    font-size: 75px;
+    line-height: 100vh;    
+`;
+
+export const SubTitle = styled.div `
+    width: 100%;
+    font-size: 20px;
+    line-height: 100vh;
+    display: flex;
+    justify-content: center;
 `;
