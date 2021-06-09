19ecfc49facadfacdcad497dd4ebb21bbd739881
KimGiHong <68697807+KimGiHong@users.noreply.github.com>
2021-06-09 15:22:40 +0900

    코드 수정

--- ezy-client/src/Components/Section1/Section1.js
+++ ezy-client/src/Components/Section1/Section1.js
@@ -1,19 +1,19 @@
 import React from "react";
-import { MainBg, MainTitle, MainSection, SubTitle, TitleSection } from "./style";
+import * as S from "./style";
 import Bg01 from '../../Assets/bg01.png';
 
 const Section1 = () => {
   return(
     <>
-      <MainSection>
-        <TitleSection>
-          <MainTitle>
+      <S.MainSection>
+        <S.TitleSection>
+          <S.MainTitle>
             EZY
-            <SubTitle>EZY는 자기 자신만의 라이프 스타일 역사를 쓰고 있습니다 😎</SubTitle>  
-          </MainTitle>
-        </TitleSection>
-        <MainBg><img className="Bg01" src={Bg01}/></MainBg>
-      </MainSection>
+            <S.SubTitle>EZY는 자기 자신만의 라이프 스타일 역사를 쓰고 있습니다 😎</S.SubTitle>  
+          </S.MainTitle>
+        </S.TitleSection>
+        <S.MainBg><img className="Bg01" src={Bg01}/></S.MainBg>
+      </S.MainSection>
     </>
   );
 };
