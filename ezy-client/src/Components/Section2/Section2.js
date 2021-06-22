c9124311b71c6e26780776c09523d3e818682471
4YJ <67637706+yejun0705@users.noreply.github.com>
2021-06-22 20:01:27 +0900

    효과 추가. 눈이 즐거워짐. 힝.

--- ezy-client/src/Components/Section2/Section2.js
+++ ezy-client/src/Components/Section2/Section2.js
@@ -2,6 +2,7 @@ import React from 'react';
 import * as S from "./style";
 import Bg11 from '../../Assets/bg11.png';
 import Bg12 from '../../Assets/bg12.png';
+import Zoom from 'react-reveal/Zoom';
 import Fade from 'react-reveal/Fade';
 import jquery from '../Section1/Jquery';
 
@@ -18,7 +19,7 @@ const Section2 = () =>  {
         </S.MainBg>
         <S.TitleSection>
           <S.MainTitle>
-            소중한 시간을 계획적으로 쓰고싶다면
+            <Zoom>소중한 시간을 계획적으로 쓰고싶다면</Zoom>
           </S.MainTitle>
         </S.TitleSection>
       </S.MainSection>
