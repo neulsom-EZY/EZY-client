40bb2ec37909876a09ecd03d2664c70c3fd0b665
4YJ <67637706+yejun0705@users.noreply.github.com>
2021-06-28 20:42:49 +0900

    섹션-1 퍼블리싱

--- src/Components/Section1/Section1.js
+++ src/Components/Section1/Section1.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import * as S from './style';
+import Img01 from '../../Assets/Img01.png';
+import Img02 from '../../Assets/Img02.png';
 
 const Section1 = () => {
   return (
     <>
       <S.MainSection>
         <S.TitleSection>
-          " 소중한 시간을 계획적으로 쓰고 싶다면 "
+          <S.MainTitle>
+            " 소중한 시간을 계획적으로 쓰고 싶다면 "
+          </S.MainTitle>
+          <S.SubTitle>
+            우리는 EZY입니다 💡<br/>
+            오직 여러분만을 위한 개인 맞춤형 일정관리 서비스를 이용하여<br/>
+            효율적으로, 효과적으로 계획한 일정을 진행해보세요 ! 
+          </S.SubTitle>
+        <img className="Img02" src={Img02} alt="Img02" />
         </S.TitleSection>
+        <img className="Img01" src={Img01} alt="Img01" />
       </S.MainSection>
     </>
   );
--- src/Components/Section1/style.js
+++ src/Components/Section1/style.js
@@ -5,12 +5,26 @@ export const MainSection = styled.div`
     width: 100%;
     height: 100vh;
     display: flex;
-    justify-content: center;
     align-items: center;
     background-image: url(${Bg01});
+    .Img01 {
+        width: 45%;
+        margin-top: -5vh;
+    }
+    .Img02 {
+        width: 22%;
+        margin-top: 4vh;
+    }
 `;
 export const TitleSection = styled.div`
-    width: auto;
-    display: flex;
-    text-align: left;
+    width: 100%;
+    margin-left: 25vh;
+`;
+export const MainTitle = styled.div`
+    font-size: 42px;
+`;
+export const SubTitle = styled.div`
+    margin-top: 4vh;
+    font-size: 20px;
+    line-height: 5vh;
 `;
