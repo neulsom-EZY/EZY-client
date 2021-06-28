3d36f642de63aa91de2de5a9347d3d517638f471
4YJ <67637706+yejun0705@users.noreply.github.com>
2021-06-28 18:04:05 +0900

    글자 섹션 추가

--- src/Components/Section1/Section1.js
+++ src/Components/Section1/Section1.js
@@ -5,6 +5,9 @@ const Section1 = () => {
   return (
     <>
       <S.MainSection>
+        <S.TitleSection>
+          " 소중한 시간을 계획적으로 쓰고 싶다면 "
+        </S.TitleSection>
       </S.MainSection>
     </>
   );
--- src/Components/Section1/style.js
+++ src/Components/Section1/style.js
@@ -4,8 +4,13 @@ import Bg01 from '../../Assets/Bg01.png';
 export const MainSection = styled.div`
     width: 100%;
     height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
     background-image: url(${Bg01});
 `;
-export const MainBg = styled.div`
-    width: 50%;
+export const TitleSection = styled.div`
+    width: auto;
+    display: flex;
+    text-align: left;
 `;
