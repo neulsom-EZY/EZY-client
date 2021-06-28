c24d6706abf3cdbf1131b8c06063f7c044683fc7
4YJ <67637706+yejun0705@users.noreply.github.com>
2021-06-28 16:57:51 +0900

    하나~ 둘~ 셋팅

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
 import { createGlobalStyle } from "styled-components";
-
+import Section1 from './Components/Section1/Section1'
 const GlobalStyle = createGlobalStyle`
   body {
         margin: 0;
         padding: 0;
         color: white;
-        background-color: #ab92ff;
     }
 `;
 
@@ -14,6 +13,7 @@ const App = () => {
   return (
     <>
       <GlobalStyle />
+      <Section1 />
     </>
   );
 };
---
+++ src/Components/Section1/Section1.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { MainSection } from '../style';
+
+const Section1 = () => {
+  return (
+    <>
+      <MainSection>
+        hello
+      </MainSection>
+    </>
+  );
+};
+
+export default Section1;
