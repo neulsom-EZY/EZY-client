835990a5e7b7f57f420b9783e2bd2d0cda9142ed
KimGiHong <68697807+KimGiHong@users.noreply.github.com>
2021-06-29 14:39:33 +0900

    img2 svg 변경

--- src/Components/Section1/Section1.js
+++ src/Components/Section1/Section1.js
@@ -18,7 +18,9 @@ const Section1 = () => {
             오직 여러분만을 위한 개인 맞춤형 일정관리 서비스를 이용하여<br/>
             효율적으로, 효과적으로 계획한 일정을 진행해보세요 ! 
           </S.SubTitle>
-        <S.Img2/>
+        <I.Img2>
+          <S.Img2/>
+        </I.Img2>
         </S.TitleSection>
         <S.Img1/>
       </S.MainSection>
