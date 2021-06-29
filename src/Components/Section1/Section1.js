9ad90d0e11193dd0bbc95650d4e3ab1c8cf77321
KimGiHong <68697807+KimGiHong@users.noreply.github.com>
2021-06-29 14:03:56 +0900

    첫 로고 svg 성공

--- src/Components/Section1/Section1.js
+++ src/Components/Section1/Section1.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import * as S from './style';
-import Img01 from '../../Assets/Img01.png';
-import Img02 from '../../Assets/Img02.png';
-import * as I from "../../Assets/index";
+import * as I from '../../Assets/index';
 
 const Section1 = () => {
   return (
     <>
       <S.MainSection>
         <S.TitleSection>
+          <S.WLogo>
+            <I.Logo/>
+          </S.WLogo>
           <S.MainTitle>
-            <S.Logo>
-              <I.Logo/>
-            </S.Logo>
             " 소중한 시간을 계획적으로 쓰고 싶다면 "
           </S.MainTitle>
           <S.SubTitle>
@@ -20,9 +18,9 @@ const Section1 = () => {
             오직 여러분만을 위한 개인 맞춤형 일정관리 서비스를 이용하여<br/>
             효율적으로, 효과적으로 계획한 일정을 진행해보세요 ! 
           </S.SubTitle>
-        <img className="Img02" src={Img02} alt="Img02" />
+        <S.Img2/>
         </S.TitleSection>
-        <img className="Img01" src={Img01} alt="Img01" />
+        <S.Img1/>
       </S.MainSection>
     </>
   );
--- src/Components/Section1/style.js
+++ src/Components/Section1/style.js
@@ -1,5 +1,8 @@
 import styled from 'styled-components';
 import Bg01 from '../../Assets/Bg01.png';
+import Logo from "../../Assets/W-Logo.png";
+import Img02 from "../../Assets/Img02.png";
+import Img01 from "../../Assets/Img01.png";
 
 export const MainSection = styled.div`
     width: 100%;
@@ -7,32 +10,60 @@ export const MainSection = styled.div`
     display: flex;
     align-items: center;
     background-image: url(${Bg01});
-    .Img01 {
-        width: 45%;
-        margin-top: -5vh;
-    }
-    .Img02 {
-        width: 22%;
-        margin-top: 4vh;
-    }
-`;
-export const TitleSection = styled.div`
-    width: 100%;
-    margin-left: 25vh;
-`;
-export const MainTitle = styled.div`
-    font-size: 42px;
-`;
-export const SubTitle = styled.div`
-    margin-top: 4vh;
-    font-size: 20px;
-    line-height: 5vh;
 `;
 
-export const Logo = styled.div`
-  svg {
-    width: 346px;
-    height: 80px;
-    overflow: visible;
-  }
+export const TitleSection = styled.div`
+    width: 100%;
+    display:grid;
+    justify-content:center;
+    align-items:center;
+`;
+export const MainTitle = styled.div`
+    font-weight: 500;
+    font-size: 40px;
+`;
+export const SubTitle = styled.div`
+    margin-top: 3vh;
+    margin-bottom: 3vh;
+    font-size: 20px;
+    line-height: 3vh;
+`;
+export const WLogo = styled.div`
+    position: fixed;
+    top: 5vh;
+    left: 16vh;
+    width: 100%;
+    height:11vh;
+    /* background-image: url(${Logo}); */
+    background-repeat: no-repeat;
+    background-size: 11vh;
+`;
+export const Img1 = styled.div`
+    width: 100%;
+    height: 130vh;
+    margin-top: -10vh;
+    -webkit-touch-callout: none;
+    -webkit-user-select: none;
+    -khtml-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    -webkit-user-drag: none;
+    background-image:url(${Img01});
+    background-repeat: no-repeat;
+    background-size: 100%;
+`;
+export const Img2 = styled.div`
+    width: 100%;
+    height: 5.1vh;
+    -webkit-touch-callout: none;
+    -webkit-user-select: none;
+    -khtml-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    -webkit-user-drag: none;
+    background-image:url(${Img02});
+    background-repeat: no-repeat;
+    background-size: 15vh;
 `;
