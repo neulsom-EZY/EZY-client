c7a7cc73c2f76556708ccecee82e5d55ac6cecd4
KimGiHong <68697807+KimGiHong@users.noreply.github.com>
2021-07-02 08:42:58 +0900

    Img05 index 추가

--- src/Assets/index.js
+++ src/Assets/index.js
@@ -3,3 +3,4 @@ export {default as Img1} from  "./Img01";
 export {default as Img2} from "./Img02";
 export {default as Img3} from  "./Img03";
 export {default as Img4} from "./Img04";
+export {default as Img5} from "./Img05";
