3e82d0c5e3d158ed9b64f90ced967df4d89e9a21
KimGiHong <68697807+KimGiHong@users.noreply.github.com>
2021-07-01 20:57:07 +0900

    4번째 svg 이미지 추가

--- src/Assets/index.js
+++ src/Assets/index.js
@@ -2,3 +2,4 @@ export {default as Logo} from "./Logo";
 export {default as Img1} from  "./Img01";
 export {default as Img2} from "./Img02";
 export {default as Img3} from  "./Img03";
+export {default as Img4} from "./Img04";
