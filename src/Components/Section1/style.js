d1756ab8b3043c7f96bf3f016ba0d3e6631b3072
4YJ <67637706+yejun0705@users.noreply.github.com>
2021-07-04 01:54:33 +0900

    PC 화면 반응형 구현 완료

--- src/Components/Section1/style.js
+++ src/Components/Section1/style.js
@@ -44,6 +44,21 @@ export const TitleSection = styled.div`
     @media screen and (max-width: 1160px) {
         width: 80%;
     }
+    @media screen and (max-width: 1070px) {
+        width: 100%;
+        margin-left: 5vh;
+    }
+    @media screen and (max-width: 940px) {
+        width: 73%;
+    }
+    @media screen and (max-width: 900px) {
+        width: 3000vh;
+        margin: 0;
+        text-align: center;
+    }
+    @media screen and (max-width: 700px) {
+        width: auto;
+    }
 `;
 export const MainTitle = styled.div`
     width: 100%;
@@ -52,6 +67,12 @@ export const MainTitle = styled.div`
     /* 13inch Display */
     @media screen and (max-width: 1680px) {
         width: 80vh;
+    }
+    @media screen and (max-width: 1050px) {
+        font-size: 35px;
+    }
+    @media screen and (max-width: 900px) {
+        margin-bottom: 4vh;
     }
 `;
 export const SubTitle = styled.div`
@@ -62,12 +83,22 @@ export const SubTitle = styled.div`
     font-size: 20px;
     font-weight: 400;
     line-height: 5vh;
+    @media screen and (max-width: 1050px) {
+        font-size: 19px;
+    }
+    @media screen and (max-width: 900px) {
+        margin: 0;
+        margin-bottom: 4vh;
+    }
 `;
 export const WLogo = styled.div`
     width: 100%;
     height: 11vh;
     margin-top: -40vh;
     margin-left: 3vh;
+    @media screen and (max-width: 900px) {
+        margin-left: -3vh;
+    }
 `;
 export const MainBg = styled.div`
     width: 100%;
@@ -116,6 +147,36 @@ export const MainBg = styled.div`
             margin-left: 10vh;
         }
     }
+    @media screen and (max-width: 1070px) {
+        .SubSubPhone {
+            margin-left: 12vh;
+        }
+    }
+    @media screen and (max-width: 1050px) {
+        .SubSubPhone {
+            margin-left: 8vh;
+        }
+    }
+    @media screen and (max-width: 1010px) {
+        .SubSubPhone {
+            margin-left: 5vh;
+        }
+    }
+    @media screen and (max-width: 980px) {
+        .SubSubPhone {
+            margin-left: 3vh;
+        }
+    }
+    @media screen and (max-width: 960px) {
+        .SubSubPhone {
+            margin-left: 0vh;
+        }
+    }
+    @media screen and (max-width: 900px) {
+        .SubSubPhone {
+            display: none;
+        }
+    }
 `;
 export const Img2 = styled.div`
     -webkit-touch-callout: none;
@@ -126,4 +187,7 @@ export const Img2 = styled.div`
     user-select: none;
     -webkit-user-drag: none;
     margin-left: 3vh;
+    @media screen and (max-width: 900px) {
+        margin: 0;
+    }
 `;
