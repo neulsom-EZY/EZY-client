daf06455129480a5e1acefe070d26ad9b808ebea
4YJ <67637706+yejun0705@users.noreply.github.com>
2021-07-15 08:30:27 +0900

    섹션-2 개발을 해보도록 하자 ~

---
+++ src/Components/Section2/Section2.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import * as S from './style';
+import * as I from './style';
+
+const Section2 = () => {
+  return (
+    <>
+      <S.MainSection>
+        
+      </S.MainSection>
+    </>
+  );
+}
