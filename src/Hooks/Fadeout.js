1cb70598822002164b49277f9f475ac85d0369b5
KimGiHong <68697807+KimGiHong@users.noreply.github.com>
2021-07-20 11:38:01 +0900

    해버렸고.

--- src/Hooks/Fadeout.js
+++ src/Hooks/Fadeout.js
@@ -2,16 +2,16 @@ const $  = require('jquery');
 
 function fade(pageLoad) {
     const windowTop = $(window).scrollTop(), windowBottom = windowTop + $(window).innerHeight();
-    const min = 0, max = 1 , threshold = 0.01;
+    const min = -0.1, max = 1 , threshold = 0.01;
 
     $(".fade").each(function () {
         const objectHeight = $(this).outerHeight(), objectTop = $(this).offset().top, objectBottom = $(this).offset().top + objectHeight;
 
         if (objectTop < windowTop) {
-            if (objectBottom > windowTop) { $(this).fadeTo(1, min + ((max - min) * ((objectBottom - windowTop) / objectHeight))); }
+            if (objectBottom >= windowTop) { $(this).fadeTo(1, min + ((max - min) * ((objectBottom - windowTop) / objectHeight))); }
             else if ($(this).css("opacity") >= min + threshold || pageLoad) { $(this).fadeTo(1, min); }
         } else if (objectBottom > windowBottom) {
-            if (objectTop < windowBottom) { $(this).fadeTo(1, min + ((max - min) * ((windowBottom - objectTop) / objectHeight))); }
+            if (objectTop <= windowBottom) { $(this).fadeTo(1, min + ((max - min) * ((windowBottom - objectTop) / objectHeight))); }
             else if ($(this).css("opacity") >= min + threshold || pageLoad) { $(this).fadeTo(1, min); }
         } else if ($(this).css("opacity") <= max - threshold || pageLoad) { $(this).fadeTo(1, max); }
     });
