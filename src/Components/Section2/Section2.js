149ce7b9c155cf7ae4fa715ded42a0e1c970e588
4YJ <67637706+yejun0705@users.noreply.github.com>
2021-07-26 18:42:59 +0900

    타이틀 섹션 완료

--- src/Components/Section2/Section2.js
+++ src/Components/Section2/Section2.js
@@ -6,7 +6,13 @@ const Section2 = () => {
     <>
       <S.MainSection>
         <S.TitleSection>
-          <S.MainTitle>당신의 일정관리의<br/>처음과 끝</S.MainTitle>
+          <S.MainTitle>
+            나를 위한 일정관리, 그 처음과 끝
+            <S.SubTitle>
+              EZY로 계획하는 오늘하루<br/>
+              EZY로 마무리하는 오늘 하루
+            </S.SubTitle>
+          </S.MainTitle>
         </S.TitleSection>
       </S.MainSection>
     </>
--- src/Components/Section2/style.js
+++ src/Components/Section2/style.js
@@ -22,13 +22,20 @@ export const TitleSection = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    text-align: center;
+    text-align: left;
 `;
 export const MainTitle = styled.div`
     width: 100%;
+    font-weight: 500;
     font-size: 40px;
     line-height: 8vh;
     text-align: left;
     margin-left: 16vh;
 `;
-export const SubTitle = styled.div``;
+export const SubTitle = styled.div`
+    width: 100%;
+    margin-top: 3vh;
+    font-size: 20px;
+    font-weight: 400;
+    line-height: 5vh;
+`;
