d4e6050e1510bae96d7678a47a96a3bf19e14a6a
KimGiHong <68697807+KimGiHong@users.noreply.github.com>
2021-07-29 16:31:14 +0900

    svg를 모두 컴포넌트화 시켜보았다

--- src/Assets/index.js
+++ src/Assets/index.js
@@ -1,4 +1,4 @@
-export {default as Logo} from "./Logo";
+// export {default as Logo} from "./Logo";
 // 첫 번째 핸드폰
 export {default as Img1} from  "./Img01";
 // App Store 버튼
@@ -8,3 +8,7 @@ export {default as Img3} from  "./Img03";
 // 세 번째 핸드폰
 export {default as Img4} from "./Img04";
 export {default as Img5} from "./Img05";
+export {default as Logo} from "./PageLogo";
+export {default as P1} from "./Phone";
+export {default as P2} from "./Phone2";
+export {default as Btn} from "./Btn";
--- src/Components/Section1/Section1.js
+++ src/Components/Section1/Section1.js
@@ -2,12 +2,8 @@ import React from 'react';
 import * as S from './style';
 import * as I from '../../Assets/index';
 import Mobile from '../../Assets/Mobile.png';
-import Btn from '../../Assets/Btn.svg';
 import Fade from 'react-reveal/Fade';
 import fadeout from "../../Hooks/Fadeout";
-import P1 from "../../Assets/Phone.svg";
-import P2 from "../../Assets/Phone2.svg";
-import Logo from "../../Assets/PageLogo.svg";
 
 const Section1 = () => {
   return (
@@ -16,7 +12,8 @@ const Section1 = () => {
         <S.TitleSection>
         {/* <S.WLogo><I.Logo/></S.WLogo> EZY 글자 들어간 로고 */}
           <img src={ Mobile } className="Mobile" alt="Mobile"/>
-          <img src={ Logo } className="Logo" alt="Logo"/>
+          {/* <img src={ Logo } className="Logo" alt="Logo"/> */}
+          <I.Logo className="Logo"/>
             <S.MainTitle>라이프스타일,<br/>
               그 이상의 역사를 만들고 있습니다</S.MainTitle>
             <S.SubTitle>
@@ -24,14 +21,17 @@ const Section1 = () => {
               오직 여러분만을 위한 개인 맞춤형 일정관리 서비스를 이용하여<br/>
               효율적으로, 효과적으로 계획한 일정을 진행해보세요 !
             </S.SubTitle>
-          <img src={ Btn } className="StoreBtn" alt="StoreBtn"/>
+            {/* <img src={ Btn } className="StoreBtn" alt="StoreBtn"/> */}
+            <I.Btn className="StoreBtn"/>
           </S.TitleSection>
           <fadeout />
           <S.MainBg className="fade">
             <Fade right>
-              <img src={ P1 } className="P1" alt="Phone"/>
+              {/* <img src={ P1 } className="P1" alt="Phone"/> */}
+              <I.P1 className="P1"/>
               <I.Img3 className="SubPhone"/>
-              <img src={ P2 } className="P2" alt="Phone2"/>
+              {/* <img src={ P2 } className="P2" alt="Phone2"/> */}
+              <I.P2 className="P2"/>
           </Fade>
         </S.MainBg>
       </S.MainSection>
