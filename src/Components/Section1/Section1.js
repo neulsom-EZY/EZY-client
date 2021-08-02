57c358cd313aa4fc1a22099e0d44c8ad7c91b736
4YJ <67637706+yejun0705@users.noreply.github.com>
2021-08-02 18:42:21 +0900

    클린 코드

--- src/Components/Section1/Section1.js
+++ src/Components/Section1/Section1.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import * as S from './style';
-import * as I from '../../Assets/index';
-import Fade from 'react-reveal/Fade';
+import React from "react";
+import * as S from "./style";
+import * as I from "../../Assets/index";
+import Fade from "react-reveal/Fade";
 import fadeout from "../../Hooks/Fadeout";
 import W_Logo from "../../Assets/WebLogo.svg";
 import W_Phone from "../../Assets/WebPhone.svg";
@@ -14,30 +14,31 @@ const Section1 = () => {
       <S.MainSection>
         <S.MobileSection>
           <S.MobileLogoSection>
-            <img src={ M_Logo } className="M-Logo" alt="Mobile-Logo"/>
+            <img src={M_Logo} className="M-Logo" alt="Mobile-Logo" />
           </S.MobileLogoSection>
           <S.MobileImgSection>
-            <img src={ M_Phone } className="M-Phone" alt="Mobile-Phone"/>
+            <img src={M_Phone} className="M-Phone" alt="Mobile-Phone" />
           </S.MobileImgSection>
         </S.MobileSection>
         <S.TitleSection>
-          <img src={ W_Logo } className="W-Logo" alt="Web-Logo"/>
-            <S.MainTitle>라이프스타일,<br/>
-              그 이상의 역사를 만들고 있습니다
-            </S.MainTitle>
-            <S.SubTitle>
-              우리는 EZY입니다<br/>
-              오직 여러분만을 위한 개인 맞춤형 일정관리 서비스를 이용하여<br/>
-              효율적으로, 효과적으로 계획한 일정을 진행해보세요 !
-            </S.SubTitle>
-            <I.Btn className="StoreBtn"/>
-          </S.TitleSection>
-          <fadeout />
-          <S.MainBg className="fade">
+          <img src={W_Logo} className="W-Logo" alt="Web-Logo" />
+          <S.MainTitle>
+            라이프스타일,<br/>
+            그 이상의 역사를 만들고 있습니다
+          </S.MainTitle>
+          <S.SubTitle>
+            우리는 EZY입니다<br/>
+            오직 여러분만을 위한 개인 맞춤형 일정관리 서비스를 이용하여<br/>
+            효율적으로, 효과적으로 계획한 일정을 진행해보세요 !
+          </S.SubTitle>
+          <I.Btn className="StoreBtn" />
+        </S.TitleSection>
+        <fadeout />
+        <S.MainBg className="fade">
           <Fade right>
-            <img src={ W_Phone } className="W-Phone" alt="Web-Phone"/>
-            <I.Img3 className="SubPhone"/>
-            <I.P2 className="P2"/>
+            <img src={W_Phone} className="W-Phone" alt="Web-Phone" />
+            <I.Img3 className="SubPhone" />
+            <I.P2 className="P2" />
           </Fade>
         </S.MainBg>
       </S.MainSection>
