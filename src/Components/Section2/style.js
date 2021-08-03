2967ee1ae2353546cd06953eecc5fd1a3ea88d88
4YJ <67637706+yejun0705@users.noreply.github.com>
2021-08-03 14:21:12 +0900

    모바일 UI - 주석 비활성화

--- src/Components/Section2/style.js
+++ src/Components/Section2/style.js
@@ -5,14 +5,12 @@ export const MainSection = styled.div`
     width: 100%;
     height: 100vh;
     z-index: -100;
+    display: flex;
+    justify-content: center;
+    align-items: center;
     background-position: center;
     background-size: cover;
     background-image: url(${Bg02});
-
-    /* display: flex;
-    justify-content: center;
-    align-items: center; */
-
     @media screen and (max-width: 900px) {
         display: flex;
         justify-content: center;
