94705d1c2430a430faa2d065292660ea3f6114b2
4YJ <67637706+yejun0705@users.noreply.github.com>
2021-08-14 16:51:54 +0900

    full-page 스크롤 제거

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { createGlobalStyle } from "styled-components";
 import Section1 from './Components/Section1/Section1';
 import Section2 from './Components/Section2/Section2';
-import {FullPage, Slide} from "react-full-page";
+import {Slide} from "react-full-page";
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -17,10 +17,8 @@ const App = () => {
   return (
     <>
       <GlobalStyle />
-      {/* <FullPage duration={1000}> */}
         <Slide><Section1/></Slide>
-        {/* <Slide><Section2/></Slide> */}
-      {/* </FullPage> */}
+        <Slide><Section2/></Slide>
     </>
   );
 };
