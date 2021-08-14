a48a3ff53864c0c3b1fcfa9574af0f15de037160
4YJ <67637706+yejun0705@users.noreply.github.com>
2021-08-14 17:41:14 +0900

    섹션-1 롤백

--- src/Components/Section1/Section1.js
+++ src/Components/Section1/Section1.js
@@ -1,8 +1,14 @@
 import React from "react";
-import '../../Hooks/Fadeout';
 import * as S from "./style";
 import * as I from "../../Assets/index";
 import Fade from "react-reveal/Fade";
+import fadeout from "../../Hooks/Fadeout";
+import W_Logo from "../../Assets/WebLogo.svg";
+import W_Phone from "../../Assets/WebPhone.svg";
+import W_StoreBtn from "../../Assets/WebStoreBtn.svg";
+import M_Logo from "../../Assets/MobileLogo.svg";
+import M_Phone from "../../Assets/MobilePhone.png";
+import M_StoreBtn from "../../Assets/MobileStoreBtn.svg";
 
 const Section1 = () => {
   return (
@@ -10,14 +16,14 @@ const Section1 = () => {
       <S.MainSection className="fade">
         <S.MobileSection>
           <S.MobileLogoSection>
-            <I.MobileLogo className="M-Logo" />
+            <img src={M_Logo} className="M-Logo" alt="Mobile-Logo" />
           </S.MobileLogoSection>
           <S.MobileImgSection>
-            <I.MobilePhone className="M-Phone fade" />
+            <img src={M_Phone} className="M-Phone fade" alt="Mobile-Phone" />
           </S.MobileImgSection>
         </S.MobileSection>
         <S.TitleSection>
-          <I.WebLogo className="W-Logo" />
+          <img src={W_Logo} className="W-Logo" alt="Web-Logo" />
           <S.MainTitle>
             라이프스타일,<br/>
             그 이상의 역사를 만들고 있습니다
@@ -27,12 +33,13 @@ const Section1 = () => {
             오직 여러분만을 위한 개인 맞춤형 일정관리 서비스를 이용하여<br/>
             효율적으로, 효과적으로 계획한 일정을 진행해보세요 !
           </S.SubTitle>
-          <I.WebStoreBtn className="W-StoreBtn" />
-          <I.MobileStoreBtn className="M-StoreBtn" />
+          <img src={W_StoreBtn} className="W-StoreBtn" alt="Web-Store-Button" />
+          <img src={M_StoreBtn} className="M-StoreBtn" alt="Mobile-Store-Button" />
         </S.TitleSection>
+        <fadeout />
         <S.MainBg className="fade">
-          <Fade right>            
-            <I.WebPhone className="W-Phone" />
+          <Fade right>
+            <img src={W_Phone} className="W-Phone" alt="Web-Phone" />
             <I.Img3 className="SubPhone" />
             <I.P2 className="P2" />
           </Fade>
