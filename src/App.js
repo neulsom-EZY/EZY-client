44fa8ec1c658dad9e0116f2f2131b4d1deda163a
4YJ <67637706+yejun0705@users.noreply.github.com>
2021-08-14 16:54:33 +0900

    병합 충돌 방지

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from "react";
 import { createGlobalStyle } from "styled-components";
 import Section1 from './Components/Section1/Section1';
 import Section2 from './Components/Section2/Section2';
-import { Slide } from "react-full-page";
 
 const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
     color: white;
-    background-color: #f9fafb;
     font-family: 'Apple SD Gothic Neo', sans-serif;
   }
 `;
@@ -18,10 +16,8 @@ const App = () => {
   return (
     <>
       <GlobalStyle />
-      {/* <FullPage duration={1000}> */}
-        <Slide><Section1/></Slide>
-        <Slide><Section2/></Slide>
-      {/* </FullPage> */}
+        <Section1/>
+        <Section2/>
     </>
   );
 };
