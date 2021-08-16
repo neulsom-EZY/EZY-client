16d1aa5dde808c2e438014b680c5ac2791d925c8
4YJ <67637706+yejun0705@users.noreply.github.com>
2021-08-16 18:38:44 +0900

    Subtitle 변경

--- src/Components/Section2/Section2.js
+++ src/Components/Section2/Section2.js
@@ -9,16 +9,18 @@ const Section2 = () => {
       <S.MainSection className="fade">
         <S.TitleSection>
           <S.MainTitle>
-            나를 위한 일정관리, 그 처음과 끝
+            나를 위한 일정관리<br />
+            그 처음과 끝
             <S.SubTitle>
-              EZY로 계획하는 오늘 하루의 시작<br/>
-              EZY로 마무리하는 오늘 하루
+              밝은 하루를 계획<br />
+              수고한 오늘 하루의 끝을<br />
+              EZY와 함께
             </S.SubTitle>
           </S.MainTitle>
         </S.TitleSection>
         <S.ImgSection className="fade">
           <Fade right>
-            <I.WebAlert className="W-Alert"/>
+            {/*<I.WebAlert className="W-Alert"/>*/}
             <S.WebSomeDay><I.WebSomeDay className="W-SomeDay"/></S.WebSomeDay>
             <S.AlertImgSection>
               <I.Content1 />
@@ -30,8 +32,8 @@ const Section2 = () => {
           </Fade>
         </S.ImgSection>
             <S.SubTitle2>
-              밝은 하루를 계획,<br/>
-              수고한 오늘 하루의 끝,<br/>
+              밝은 하루를 계획,<br />
+              수고한 오늘 하루의 끝,<br />
               EZY와 함께.
             </S.SubTitle2>
       </S.MainSection>
