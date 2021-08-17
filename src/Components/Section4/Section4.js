bf3b3e4d115d13cb5ea7030a92f5ae61240ab39b
KimGiHong <68697807+KimGiHong@users.noreply.github.com>
2021-08-17 21:45:28 +0900

    섹션4 틀을 잡아보았다 이제 svg를 잘라야 된다

--- src/Components/Section4/Section4.js
+++ src/Components/Section4/Section4.js
@@ -6,7 +6,48 @@ const Section4 = () => {
     return(
         <>
             <S.MainSection>
-                
+                <S.TitleSection>
+                    <S.MainTitle>
+                        오직 당신만을 위한 <br/>
+                        이런 TodoList 써보셨나요?
+                    </S.MainTitle>
+                </S.TitleSection>
+                <S.ImgSection>
+                    <S.FirstSection>
+                        <S.SubTitle>
+                            언제던지 <br/>
+                            간편한 일정관리
+                        </S.SubTitle>
+                    </S.FirstSection>
+                    <S.SecondSection>
+                        <S.SubSection>
+                            <S.SubTitle>
+                                오늘의 일정을 <br/>
+                                간단하게 확인
+                            </S.SubTitle>
+                        </S.SubSection>
+                        <S.SubSection2>
+                            <S.SubSection3>
+                                <S.Emoji>
+                                    😑
+                                </S.Emoji>
+                                <S.SubTitle2>
+                                    일정을<br/>
+                                    눈감고도<br/>
+                                    확인
+                                </S.SubTitle2>
+                            </S.SubSection3>
+                            <S.SubSection4>
+                                <S.Emoji>
+                                    👀
+                                </S.Emoji>
+                                <S.SubTitle2>
+                                    이제 눈 떠요!
+                                </S.SubTitle2>
+                            </S.SubSection4>
+                        </S.SubSection2>
+                    </S.SecondSection>
+                </S.ImgSection>
             </S.MainSection>
         </>
     )
--- src/Components/Section4/style.js
+++ src/Components/Section4/style.js
@@ -2,11 +2,106 @@ import styled from 'styled-components';
 
 export const MainSection = styled.div`
     width: 100%;
-    height: 100vh;
+    height: 150vh;
     z-index: -100;
-    display: flex;
+    display: grid;
     justify-content: center;
     align-items: center;
     background-position: center;
     background-size: cover;
+`;
+
+export const TitleSection = styled.div`
+    width: 100%;
+    word-break: keep-all;
+`;
+
+export const MainTitle = styled.div`
+    width: 100%;
+    font-weight: bold;
+    font-size: 44px;
+    line-height: 6vh;
+    color: black;
+`;
+
+export const SubTitle = styled.div`
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    font-size: 35px;
+    font-weight: bold;
+    line-height: 6vh;
+    color: black;
+`;
+export const ImgSection = styled.div`
+    display:grid;
+    align-items:center;
+    justify-content:center;
+    width: 100%;
+    height:100%;
+    margin-top: -20vh;
+`;
+
+export const FirstSection = styled.div`
+    display:flex;
+    align-items:center;
+    justify-content:center;
+    background-color: #BAC8FF;
+    width: 100vh;
+    height: 47vh;
+    border-radius: 6vh;
+`;
+
+export const SecondSection = styled.div`
+    display:flex;
+    align-items:center;
+    justify-content:center;
+    width: 100vh;
+    height: 47vh;
+`;
+
+export const SubSection = styled.div`
+    width: 75vh;
+    height: 100%;
+    background-color: #AFADFF;
+    border-radius: 6vh;
+`;
+
+export const SubSection2 = styled.div`
+    width: 25vh;
+    height: 100%;
+    margin-left: 4vh;
+`;
+
+export const SubSection3 = styled.div`
+    display:grid;
+    width: 100%;
+    height: 70%;
+    background-color: #FFB5B5;
+    border-radius: 6vh;
+    margin-bottom: 1vh;
+`;
+
+export const Emoji = styled.div`
+    display:flex;
+    justify-content:center;
+    align-items: center;
+    font-size: 7vh;
+`;
+
+export const SubTitle2 = styled.div`
+    font-size: 35px;
+    font-weight: bold;
+    line-height: 4vh;
+    color: black;
+    float: left;
+    margin-left: 4vh;
+`;
+
+export const SubSection4 = styled.div`
+    width: 100%;
+    height: 30%;
+    background-color: #E2F6D7;
+    border-radius: 4vh;
 `;
