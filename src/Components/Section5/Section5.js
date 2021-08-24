a9ad6aaa7d13d2e2ccf168c74b1fa82f529cae28
KimGiHong <68697807+KimGiHong@users.noreply.github.com>
2021-08-24 09:27:22 +0900

    모바일 디자인 기다려야징

--- src/Components/Section5/Section5.js
+++ src/Components/Section5/Section5.js
@@ -13,8 +13,62 @@ const Section5 = () => {
                     </S.MainTitle>
                 </S.TitleSection>
                 <S.ReviewSection>
-                    
-                    <S.BgCircle />
+                    <S.Review1>
+                        <S.ReviewTitle>
+                            팀 일정을 팀원 모두가<br/>
+                            알 수 있어서 소통이 잘 돼요!!
+                        </S.ReviewTitle>
+                        <S.ReviewBtn1>
+                            <S.BtnTitle>
+                                #팀일정
+                            </S.BtnTitle>
+                        </S.ReviewBtn1>
+                    </S.Review1>
+                    <S.Review2>
+                        <S.ReviewTitle>
+                        알아서 시간에 맞춰서 일정을 알려주니까<br/>
+                        깜빡하지 않아서 좋아요
+                        </S.ReviewTitle>
+                        <S.ReviewBtn2>
+                            <S.BtnTitle>
+                                #일정관리
+                            </S.BtnTitle>
+                        </S.ReviewBtn2>
+                    </S.Review2>
+                    <S.Review3>
+                        <S.ReviewTitle>
+                            친구 생일 파티할 때<br/>
+                            케이크를 깜빡한 적이 있는데,<br/>
+                            약속 장소로 오던 다른 친구에게 부탁할 수 있었어요
+                        </S.ReviewTitle>
+                        <S.ReviewBtn3>
+                            <S.BtnTitle>
+                                #심부름
+                            </S.BtnTitle>
+                        </S.ReviewBtn3>
+                    </S.Review3>
+                    <S.Review4>
+                        <S.ReviewTitle>
+                            여러명이서도 계획한 일정들을<br/>
+                            간편하게 공유하고, 확인할 수 있어서 좋았습니다.
+                        </S.ReviewTitle>
+                        <S.ReviewBtn4>
+                            <S.BtnTitle>
+                                #팀일정
+                            </S.BtnTitle>
+                        </S.ReviewBtn4>
+                    </S.Review4>
+                    <S.Review5>
+                        <S.ReviewTitle>
+                            친구 부려먹기가 너무 좋네요 :)
+                        </S.ReviewTitle>
+                        <S.ReviewBtn3>
+                            <S.BtnTitle>
+                                #심부름
+                            </S.BtnTitle>
+                        </S.ReviewBtn3>
+                    </S.Review5>
+                    <S.BgCircle/>
                 </S.ReviewSection>
             </S.MainSection>
         </>
--- src/Components/Section5/style.js
+++ src/Components/Section5/style.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const MainSection = styled.div`
     width: 100%;
-    height: 100vh;
+    height: 120vh;
     z-index: -100;
     display: grid;
     justify-content: center;
@@ -12,7 +12,7 @@ export const MainSection = styled.div`
 `;
 
 export const TitleSection = styled.div`
-    width: 100%;
+    width: 65vw;
     word-break: keep-all;
 `;
 
@@ -27,11 +27,127 @@ export const MainTitle = styled.div`
 export const ReviewSection = styled.div`
     width: 100%;
     height:100%;
+    margin-top: 20vh;
+    display: grid;
+    justify-content:center;
+    align-items:center;
 `;
 
 export const BgCircle = styled.div`
     width: 50vh;
     height: 50vh;
+    position: relative;
+    bottom: 120vh;
     border-radius: 50%;
     background-color: #AFADFF;
+`;
+
+export const Review1 = styled.div`
+    width: 40vh;
+    height: 22vh;
+    position: relative;
+    top: 1vh;
+    right: 13vh;
+    background-color: white;
+    box-shadow: 1vh 1vh 4vh #adb5bd;
+    z-index: 3;
+`;
+
+export const Review2 = styled.div`
+    width: 43vh;
+    height: 25vh;
+    position: relative;
+    bottom: 13vh;
+    left: 35vh;
+    background-color: white;
+    box-shadow: 1vh 1vh 4vh #adb5bd;
+    z-index: 1;
+`;
+
+export const Review3 = styled.div`
+    width: 45vh;
+    height: 25vh;
+    position: relative;
+    bottom: 35vh;
+    right: 35vh;
+    background-color: white;
+    box-shadow: 1vh 0.1vh 4vh #adb5bd;
+    z-index: 2;
+`;
+
+export const Review4 = styled.div`
+    width: 45vh;
+    height: 26vh;
+    position: relative;
+    bottom: 40vh;
+    right: 15vh;
+    background-color: white;
+    box-shadow: 1vh 1vh 4vh #adb5bd;
+    z-index: 1;
+`;
+
+export const Review5 = styled.div`
+    width: 40vh;
+    height: 22vh;
+    position: relative;
+    bottom: 55vh;
+    left: 20vh;
+    background-color: white;
+    box-shadow: 1vh 1vh 4vh #adb5bd;
+    z-index: 2;
+`;
+
+export const ReviewTitle = styled.div`
+    width: 100%;
+    margin: 3vh 0 0 3vh;
+    font-size: 20px;
+    font-weight: Medium;
+    color: black;
+    z-index: 3;
+`;
+
+export const ReviewBtn1 = styled.button`
+    width: 11vh;
+    height: 3vh;
+    margin: 9vh 0 0 3vh;
+    background-color: #BAC8FF;
+    border-radius: 5vh;
+    border: 0;
+    outline: 0;
+`;
+
+export const ReviewBtn2 = styled.button`
+    width: 11vh;
+    height: 3vh;
+    margin: 12vh 0 0 3vh;
+    background-color: #E2F6D7;
+    border-radius: 5vh;
+    border: 0;
+    outline: 0;
+`;
+
+export const ReviewBtn3 = styled.button`
+    width: 11vh;
+    height: 3vh;
+    margin: 10vh 0 0 3vh;
+    background-color: #FFB5B5;
+    border-radius: 5vh;
+    border: 0;
+    outline: 0;
+`;
+
+export const ReviewBtn4 = styled.button`
+    width: 11vh;
+    height: 3vh;
+    margin: 13vh 0 0 3vh;
+    background-color: #BAC8FF;
+    border-radius: 5vh;
+    border: 0;
+    outline: 0;
+`;
+
+export const BtnTitle = styled.div`
+    width: 100%;
+    font-size: 18px;
+    font-weight: 500;
 `;
