5c13426b9c2e16d729a3193b5663fae80f44b659
KimGiHong <68697807+KimGiHong@users.noreply.github.com>
2021-08-26 17:37:56 +0900

    복잡한 import 코드들을 없애버렸다

--- src/Components/Section2/Section2.js
+++ src/Components/Section2/Section2.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import Fade from 'react-reveal/Fade';
 import * as S from './style';
-import Alert from '../../Assets/Section2/Alert.png';
-import SomeDay from '../../Assets/Section2/SomeDay.png';
-import Block1 from '../../Assets/Section2/Block-1.png';
-import Block2 from '../../Assets/Section2/Block-2.png';
-import Block3 from '../../Assets/Section2/Block-3.png';
-import Block4 from '../../Assets/Section2/Block-4.png';
 
 const Section2 = () => {
   return (
@@ -25,13 +19,13 @@ const Section2 = () => {
         </S.TitleSection>
         <S.ImgSection className="fade">
           <Fade right>
-            <img src={Alert} className="Alert" alt="Alert-Phone-Image" />
-            <img src={SomeDay} className="SomeDay" alt="SomeDay-PHone-Image" />
+            <img src={require('../../Assets/Section2/Alert.png' )} className="Alert" alt="Alert-Phone-Img" />
+            <img src={require('../../Assets/Section2/SomeDay.png' )} className="SomeDay" alt="SomeDay-PHone-Img"/>
             <S.AlertImgSection>
-              <img src={Block1} className="Block-1" alt="Block-Image" />
-              <img src={Block2} className="Block-2" alt="Block-Image" />
-              <img src={Block3} className="Block-3" alt="Block-Image" />
-              <img src={Block4} className="Block-4" alt="Block-Image" />
+              <img src={require('../../Assets/Section2/Block-1.png')} className="Block-1" alt="Block-Img" />
+              <img src={require('../../Assets/Section2/Block-2.png')} className="Block-2" alt="Block-Img" />
+              <img src={require('../../Assets/Section2/Block-3.png')} className="Block-3" alt="Block-Img" />
+              <img src={require('../../Assets/Section2/Block-4.png')} className="Block-4" alt="Block-Img" />
             </S.AlertImgSection>
           </Fade>
         </S.ImgSection>
