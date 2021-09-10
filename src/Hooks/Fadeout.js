59ff14e2ebc1131ebacbf9b82c8b7257407a117a
4YJ <ohman0705@naver.com>
2021-09-10 15:31:29 +0900

    Fade Query 코드 정리

--- src/Hooks/Fadeout.js
+++ src/Hooks/Fadeout.js
@@ -1,22 +1,35 @@
-const $  = require('jquery');
+const $ = require('jquery');
 
 function FadeOut(pageLoad) {
-    const windowTop = $(window).scrollTop(), windowBottom = windowTop + $(window).innerHeight();
-    const min = -0.5, max = 2.5, threshold = 0.01;
+  const windowTop = $(window).scrollTop(), windowBottom = windowTop + $(window).innerHeight();
+  const min = -0.5, max = 2.5, threshold = 0.01;
 
-    $(".fade").each(function () {
-        const objectHeight = $(this).outerHeight(), objectTop = $(this).offset().top, objectBottom = $(this).offset().top + objectHeight;
+  $(".fade").each(function () {
+    const objectHeight = $(this).outerHeight(), objectTop = $(this).offset().top,
+        objectBottom = $(this).offset().top + objectHeight;
 
-        if (objectTop < windowTop) {
-            if (objectBottom >= windowTop) { $(this).fadeTo(1, min + ((max - min) * ((objectBottom - windowTop) / objectHeight))); }
-            else if ($(this).css("opacity") >= min + threshold || pageLoad) { $(this).fadeTo(1, min); }
-        } else if (objectBottom > windowBottom) {
-            if (objectTop <= windowBottom) { $(this).fadeTo(1, min + ((max - min) * ((windowBottom - objectTop) / objectHeight))); }
-            else if ($(this).css("opacity") >= min + threshold || pageLoad) { $(this).fadeTo(1, min); }
-        } else if ($(this).css("opacity") <= max - threshold || pageLoad) { $(this).fadeTo(1, max); }
-    });
-} FadeOut(true);
-$(window).scroll(function () { FadeOut(false); });
+    if (objectTop < windowTop) {
+      if (objectBottom >= windowTop) {
+        $(this).fadeTo(1, min + ((max - min) * ((objectBottom - windowTop) / objectHeight)));
+      } else if ($(this).css("opacity") >= min + threshold || pageLoad) {
+        $(this).fadeTo(1, min);
+      }
+    } else if (objectBottom > windowBottom) {
+      if (objectTop <= windowBottom) {
+        $(this).fadeTo(1, min + ((max - min) * ((windowBottom - objectTop) / objectHeight)));
+      } else if ($(this).css("opacity") >= min + threshold || pageLoad) {
+        $(this).fadeTo(1, min);
+      }
+    } else if ($(this).css("opacity") <= max - threshold || pageLoad) {
+      $(this).fadeTo(1, max);
+    }
+  });
+}
+
+FadeOut(true);
+$(window).scroll(function () {
+  FadeOut(false);
+});
 
 
 export default FadeOut;
