4817faeb08ec9ae0eec6f6fc092d0cc4705b0dc2
4YJ <ohman0705@naver.com>
2021-09-10 14:27:03 +0900

    Title/Block 간격 줄임

--- src/Components/Section4/style.js
+++ src/Components/Section4/style.js
@@ -24,7 +24,7 @@ export const TitleSection = styled.div`
     width: 100%;
     word-break: keep-all;
     @media screen and (max-width: 960px)
-    { width: 50% } 
+    { width: 50%; margin-bottom: -20vh} 
     @media screen and (max-width: 700px)
     { width: 100% }
 `;
