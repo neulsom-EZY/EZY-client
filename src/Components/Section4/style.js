8f0c162fe23ff97178ef05784ebcdfd9425d49f7
yoosion <rsy5487@naver.com>
2021-10-02 16:21:34 +0900

    그림자 추가

--- src/Components/Section4/style.js
+++ src/Components/Section4/style.js
@@ -139,6 +139,7 @@ export const SubSection = styled.div`
     border-radius: 6vh;
     background-color: #FFFFFF;
     margin-top: 1vh;
+    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
     .Bottom {width: 40vh; margin-left: 19vh};
     @media screen and (max-width: 960px) { 
       width: 50vh;
@@ -175,6 +176,7 @@ export const SubSection3 = styled.div`
     border-radius: 5vh;
     background-color: #FFFFFF;
     margin-bottom: 1vh;
+    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
     @media screen and (max-width: 700px) { 
       width: 100%; height: 60%; 
       text-align: center; 
