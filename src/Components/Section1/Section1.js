ff05b28de63a260517ecb2db06da61910c88ea3c
4YJ <ohman0705@naver.com>
2021-10-07 20:53:59 +0900

    mainSection 완료

--- src/Components/Section1/Section1.js
+++ src/Components/Section1/Section1.js
@@ -1,6 +1,7 @@
 import React from "react";
 import * as S from "./style";
 import PC from "../../Assets/Section1/PC.mp4";
+import Logo from "../../Assets/Section1/Logo"
 
 const Section1 = () => {
   return (
@@ -8,10 +9,11 @@ const Section1 = () => {
         <S.MainSection>
           <S.VideoSection>
             <S.TitleSection>
+              <S.LogoSection><Logo/></S.LogoSection>
               여러분의<br/>라이프스타일을<br/>혁신합니다.
             </S.TitleSection>
             <video muted autoPlay loop>
-              <source src={PC} type="video/mp4" />
+              <source src={PC} type="video/mp4"/>
             </video>
           </S.VideoSection>
         </S.MainSection>
--- src/Components/Section1/style.js
+++ src/Components/Section1/style.js
@@ -14,6 +14,11 @@ export const TitleSection = styled.div`
   z-index: 1;
   margin-left: 15vh;
 `;
+export const LogoSection = styled.div`
+  top: -135%;
+  position: absolute;
+  z-index: 1;
+`;
 export const SubSection = styled.div`
   width: 100%;
   height: 50vh;
