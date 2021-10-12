65a183c7e91b372d16c8c5033633eeb7e420af24
4YJ <ohman0705@naver.com>
2021-10-12 09:31:36 +0900

    PC 완료

--- src/Components/Section2/Section2.js
+++ src/Components/Section2/Section2.js
@@ -2,12 +2,24 @@ import React from "react";
 import Fade from "react-reveal/Fade";
 import * as S from "./style";
 import Hand from "../../Assets/Section2/hand.png";
+import Phone from "../../Assets/Section2/Phone.png";
 
 const Section2 = () => {
   return (
     <>
       <S.MainSection>
-        
+        <S.ImgSection>
+          <Fade left><img src={Hand} className="Hand" alt="hand"/></Fade>
+          <Fade top><img src={Phone} className="Phone" alt="phone"/></Fade>
+        </S.ImgSection>
+        <S.TitleSection>
+          <S.MainTitle>어떤 하루를<br/>보내실 건가요?</S.MainTitle>
+          <S.SubTitle>
+            일정을 추가해보세요.<br/>
+            약속된 일정에 늦지 않도록,<br/>
+            저희가 미리 알려줄게요.
+          </S.SubTitle>
+        </S.TitleSection>
       </S.MainSection>
     </>
   );
--- src/Components/Section2/style.js
+++ src/Components/Section2/style.js
@@ -3,4 +3,51 @@ import styled from 'styled-components';
 export const MainSection = styled.div`
   width: 100%;
   height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+export const ImgSection = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  margin-left: -17vh;
+  .Phone {
+    width: 40vh;
+    height: 80vh;
+    margin-left: -10vh;
+    z-index: -1;
+  }
+  .Hand {
+    width: 60vh;
+    height: 47vh;
+    z-index: 0;
+    margin-top: 9vh;
+  }
+`;
+export const TitleSection = styled.div`
+  width: 100%;
+  margin-right: 8vh;
+  display: grid;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  word-break: keep-all;
+`;
+export const MainTitle = styled.div`
+  width: 100%;
+  font-weight: bold;
+  font-size: 55px;
+  letter-spacing: 0.1vh;
+  line-height: 8.5vh;
+  color: black;
+`;
+export const SubTitle = styled.div`
+  width: 100%;
+  margin-top: 6vh;
+  font-size: 30px;
+  font-weight: 400;
+  line-height: 6vh;
+  color: #656565;
 `;
