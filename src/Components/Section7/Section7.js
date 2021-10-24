6c1ee7938e3a1914d511031471b9f93b62e08b69
yoosion <rsy5487@naver.com>
2021-10-24 23:26:52 +0900

    버튼 추가

--- src/Components/Section7/Section7.js
+++ src/Components/Section7/Section7.js
@@ -1,6 +1,6 @@
 import React from "react";
 import * as S from "./style";
-
+import StoreBtn from "../../Assets/Section7/StoreBtn.png";
 
 const Section1 = () => {
   return (
@@ -10,6 +10,9 @@ const Section1 = () => {
                 <S.MainTitle>
                     세상에 없던 일정관리, EZY를 만나보세요.
                 </S.MainTitle>
+                <S.ImgSection>
+                    <img src={StoreBtn} />
+                </S.ImgSection>
             </S.TitleSection>
         </S.MainSection>
         <S.SubSection>
--- src/Components/Section7/style.js
+++ src/Components/Section7/style.js
@@ -1,23 +1,31 @@
 import styled from "styled-components";
 
 export const MainSection = styled.div`
-  height: 100vh;
+  height: 70vh;
   display: flex; 
   justify-content: center;
   align-items: center;
-  overflow-x: hidden;
 `;
 
 export const TitleSection = styled.div`
-    /* background-color: red; */
 `;
 
 export const MainTitle = styled.div`
     font-size: 50px;
-    background: linear-gradient(to right, #6B40FF , #1F1AFF , #221EFF ,#8099FF );
+    background: linear-gradient(to left ,#6B40FF , #A397FF, #8099FF, #6355CE);
+    /* background: linear-gradient(to right, #6B40FF , #1F1AFF , #221EFF ,#8099FF ); */
     -webkit-background-clip: text;
     -webkit-text-fill-color: transparent;
     font-weight: bold;
+`;
+
+export const ImgSection = styled.div`
+    padding-top: 100px;
+    img {
+        display: block;
+        margin: 0 auto;
+        cursor: pointer;
+    }
 `;
 
 export const SubSection = styled.div`
@@ -26,23 +34,20 @@ export const SubSection = styled.div`
     font-size: 35px;
     font-weight: 500;
     line-height: 8vh;
-    overflow-x: hidden;
-
     `;
 
 export const Line = styled.div`
-    width: 100vw;
     height: 3px;
     background: linear-gradient(90deg, rgba(107, 64, 255, 0.5) 0%, rgba(31, 26, 255, 0.5) 49.48%, rgba(34, 30, 255, 0.5) 49.49%, rgba(128, 153, 255, 0.5) 100%);
 `;
 
 export const LineText = styled.div`
-    width: 100vw;
     font-size: 20px;
     background: linear-gradient(to right, #6B40FF 70%, #1F1AFF 70%, #221EFF 70%,#8099FF 70%);
     -webkit-background-clip: text;
     -webkit-text-fill-color: transparent;
     line-height: 36px;
-    margin-left: 520px;
+    margin-left: 515px;
     margin-top: 50px;
 `;
+
