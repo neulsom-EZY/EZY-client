05ef17296f834849983b4b5c4ea334492cb85b60
4YJ <ohman0705@naver.com>
2021-10-27 16:07:12 +0900

    BlockSection 이미지 추가

--- src/Components/Section4/Section4.js
+++ src/Components/Section4/Section4.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import * as S from './style';
 import Fade from 'react-reveal/Fade';
-// import Top from '../../Assets/Section4/Top.png';
-// import Bottom from '../../Assets/Section4/Bottom.png';
+import Top from '../../Assets/Section4/Top.png';
+import Bottom from '../../Assets/Section4/Bottom.png';
 
 const Section4 = () => {
   return (
