348c7fec2d21c5889ce7296e8bd150ca2adfbdc4
KimGiHong <68697807+KimGiHong@users.noreply.github.com>
2021-10-30 16:46:45 +0900

    Footer Fix

--- src/Components/Section7/style.js
+++ src/Components/Section7/style.js
@@ -5,7 +5,7 @@ export const MainSection = styled.div`
   display: grid;
   justify-content: center;
   flex-direction: column;
-  overflow-x: hidden;
+  overflow: hidden;
   background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
   background-size: 400% 400%;
   animation: gradient 3s ease infinite;
@@ -18,6 +18,7 @@ export const TitleSection = styled.div`
   display: flex;
   justify-content: center;
   flex-direction: column;
+  margin-bottom: -20vh;
 `;
 
 
@@ -27,7 +28,7 @@ export const MainTitle = styled.div`
   font-weight: bold;
   text-align: center;
   line-height: 8vh;
-
+  margin-bottom: -20vh;
   @media screen and (max-width: 1024px) {
     width: 500px;
     text-align: center;
@@ -36,6 +37,7 @@ export const MainTitle = styled.div`
     font-size: 40px;
     width: 400px;
     line-height: 7.5vh;
+    margin-bottom: -50vh;
   }
   @media screen and (max-width: 400px) {
     font-size: 35px;
