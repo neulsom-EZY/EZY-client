23ff93d718661ce6880ae983020150d240a16af6
4YJ <ohman0705@naver.com>
2021-10-30 17:04:34 +0900

    Section1 height 오류

--- src/Components/Section1/style.js
+++ src/Components/Section1/style.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const MainSection = styled.div`
   width: 100%;
-  height: 100vh;
+  height: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
